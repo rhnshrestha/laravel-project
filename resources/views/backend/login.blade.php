5af8a7e4492261a3c8992c916b2e4576f4d1dcd5
rhnshrestha <shresthrohan447@gmail.com>
2025-05-28 13:34:38 +0545

    used @extend, @section(''), @endsection for dynamic paging

--- resources/views/backend/login.blade.php
+++
@@ -1,1 +0,0 @@
-@extends('backend.layout.auth_master');
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
-    return view('backend.login');
+    return view('backend.user.login');
+});
+
+Route::get('/register', function () {
+    return view('backend.user.register');
+});
+
+Route::get('/forgot-password', function () {
+    return view('backend.user.forgot_password');
 });
